dfb52bf2d81a5d795c9023c24dc7048ed5168939
Ændrew Rininsland <aendrew@aendrew.com>
2013-05-15 12:22:43 +0100

    Added project template.

---
+++ templates/project_templates.rb
@@ -0,0 +1,11 @@
+require 'ruble'
+ 
+project_template "Twitter Bootstrap" do |t|
+  t.type = :web
+  t.tags = ['Web']
+  t.id = "com.aptana.project.template.web.twitterbootstrap"
+  t.location = "git://github.com/bowerjs/bootstrap.git"
+  t.description = "Template based on Twitter Bootstrap. Requires network access."
+  t.icon = "https://raw.github.com/twitter/bootstrap/gh-pages/assets/img/bootstrap-docs-readme.png"
+  t.tags = ['Web']
+end
